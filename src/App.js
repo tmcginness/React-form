f6ad2ecb5f432b81c86fb210f2ec34d2c92ccbf5
Theodore McGinness <ttmcginness22@gmail.com>
2018-06-14 15:54:30 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import Projects from './Components/Projects';
+import Todos from './Components/Todos';
+import uuid from 'uuid';
+import AddProject from './Components/AddProject';
+import $ from 'jquery';
+import './App.css';
+
+
+
+
+class App extends Component {
+constructor(){
+  super();
+  this.state = {
+    projects: [],
+    todos:[]
+  }
+}
+
+getToDos(){
+$.ajax({
+  url: 'https://jsonplaceholder.typicode.com/todos',
+  dataType: 'json',
+  cache: false,
+  success: function(data){
+    this.setState({todos: data}, function(){
+      console.log(this.state);
+    })
+  }.bind(this),
+  error: function(xhr, status, err){
+    console.log(err);
+  }
+})
+}
+
+getProjects(){
+  this.setState({projects: [
+    {
+      id:uuid.v4(),
+      title: 'Business Website',
+      category: 'Web Design'
+      },
+      {
+        id:uuid.v4(),
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        id:uuid.v4(),
+        title: 'eCommerce Shopping Cart',
+        category: 'Web Development'
+      }
+  ]});
+}
+componentWillMount(){
+  this.getProjects();
+  this.getToDos();
+}
+
+componentDidMount(){
+  this.getToDos();
+}
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects})
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+  render() {
+    return (
+      <div className="App">
+        <AddProject addProject={this.handleAddProject.bind(this)}/>
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+        <hr />
+        <Todos todos={this.state.todos}/>
+      </div>
+    )
+  }
+}
+
+export default App;
